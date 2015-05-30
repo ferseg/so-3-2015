bb8a52126cbe99be18561c33f3e29fe52bbd1995
Fernando Segovia <fernandosegovia25@gmail.com>
2015-05-30 17:51:00 -0600

    Pruebas

--- FileSystem/src/main/Main.java
+++ FileSystem/src/main/Main.java
@@ -5,7 +5,6 @@
  */
 package main;
 
-import java.util.Arrays;
 import java.util.Map.Entry;
 import model.structure.Archive;
 import model.structure.File;
@@ -27,22 +26,26 @@ public class Main {
         Folder subFolder = new Folder(folder, "IS");
         Folder subFolder2 = new Folder(subFolder, "Projects");
         Archive archive = new Archive(subFolder2, "privado.txt");
+        
         archive.addLine(1, "Hello");
         archive.addLine(5, "Wold!");
         archive = (Archive) folder.getFile("IS/Projects/privado.txt");
         String path = archive.getPath();
         System.out.println(path);
-        archive.renameFile("archivito");
-        archive = (Archive) folder.getFile("IS/Projects/archivito");
-        path = archive.getPath();
-        System.out.println(path);
-        if(archive != null) {
-            for(Entry<Integer, String> actual : archive.getContent()) {
-                System.out.println("Sector : " + actual.getKey() + ", content: " + actual.getValue());
+        archive = new Archive(subFolder2, "archivito");
+        archive = new Archive(subFolder2, "archivito2");
+        subFolder = new Folder(subFolder2, "Private");
+        subFolder = new Folder(subFolder, "Mine");
+        archive = new Archive(subFolder, "Some.txt");
+        Folder res = (Folder) folder.getFile("IS/Projects/Private");
+        System.out.println(res.getPath());
+        for(Entry<String, File> actual : res.getContent()) {
+            String name = actual.getKey();
+            File value = actual.getValue();
+            if(value instanceof Folder) {
+                System.out.print("Folder ");
             }
-        }
-        else {
-            System.out.println("El archivo no se encuentra");
+            System.out.println("Name : " + name + ", content: " + value);
         }
         
     }
